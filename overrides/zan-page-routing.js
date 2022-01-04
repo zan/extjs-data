809c740bac993a6cd65f4a1579c2860a2b389be9
Alex Dean <alex@zanconsulting.com>
2022-01-03 17:13:13 -0700

    re-organize files to work around import issues and make more sense

--- overrides/zan-page-routing.js
+++
@@ -1,9 +0,0 @@
-Ext.require(
-    [
-        'Zan.data.controller.ZanPackageRoutingController',
-    ],
-    function() {
-        // Initialize package routing
-        Ext.create('Zan.data.controller.ZanPackageRoutingController');
-    }
-);
