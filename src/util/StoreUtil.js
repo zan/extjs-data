8ef8ba6dd86a887b0a90f4fa9fec92cf691c4a24
Alex Dean <alex@zanconsulting.com>
2022-01-11 12:13:40 -0700

    Add StoreUtil

---
+++ src/util/StoreUtil.js
@@ -0,0 +1,43 @@
+/**
+ * Utility methods for working with stores
+ */
+Ext.define('Zan.data.util.StoreUtil', {
+    singleton: true,
+
+    /**
+     * Synchronizes the store's records to match the records in setRecords
+     *
+     * This method ensures that the store's remove() and add() methods are called so that store methods like
+     * getRemovedRecords() work as expected
+     */
+    setRecords: function(store, setRecords) {
+        var toAdd = [];
+        var toRemove = [];
+
+        Ext.Array.forEach(setRecords, function(setRecord) {
+            // Nothing to do if the record already exists in the store
+            // todo: support records with alternate ID fields
+            if (store.findRecord('id', setRecord.getId())) return true;
+
+            // Not found in store, it needs to be added
+            toAdd.push(setRecord);
+        });
+
+        // Removed records will be ones in the store but not in the setRecords() array
+        store.each(function(storeRecord) {
+            var found = false;
+
+            for (var i=0; i < setRecords.length; i++) {
+                if (setRecords[i].getId() === storeRecord.getId()) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) toRemove.push(storeRecord);
+        });
+
+        store.remove(toRemove);
+        store.add(toAdd);
+    },
+});
