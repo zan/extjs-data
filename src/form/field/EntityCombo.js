dd912f1a8897cc57b497f1778b23529954149414
Alex Dean <alex@zanconsulting.com>
2022-01-11 12:34:00 -0700

    EntityCombo now has zanRecordValue support for writing its value back to a record field

--- src/form/field/EntityCombo.js
+++ src/form/field/EntityCombo.js
@@ -1,6 +1,19 @@
 Ext.define('Zan.data.form.field.EntityCombo', {
     extend: 'Ext.form.field.ComboBox',
 
+    requires: [
+        'Zan.data.util.ModelUtil',
+    ],
+
+    config: {
+        /**
+         * @cfg {Zan.data.model.EntityModel} Record that this component should write its value to
+         *
+         * Note that this requires specifying a 'name' as well to determine which field/association should be updated
+         */
+        zanValueRecord: null,
+    },
+
     displayField: 'label',
     valueField: 'id',
 
@@ -23,5 +36,27 @@ Ext.define('Zan.data.form.field.EntityCombo', {
             cls: 'x-form-clear-trigger',
             handler: Ext.form.field.ComboBox.prototype.clearValue,
         }
-    }
+    },
+
+    initComponent: function() {
+        this.callParent(arguments);
+
+        this.on('change', this._onChange, this);
+    },
+
+    updateZanValueRecord: function(record) {
+        if (!this.getName()) {
+            throw new Error("This field must have a 'name' set to use zanValueRecord");
+        }
+
+        this.setValue(record.zanGet(this.getName()));
+    },
+
+    _onChange: function(combo, newValue, oldValue, opts) {
+        if (!this.getZanValueRecord()) return;
+
+        var recordValue = this.getSelection();
+
+        Zan.data.util.ModelUtil.setIfDifferent(this.getZanValueRecord(), this.getName(), recordValue);
+    },
 });
