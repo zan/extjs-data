1deb841f1852eb2c6df23b863dfc89ecb99fbc41
Alex Dean <alex@zanconsulting.com>
2022-01-18 16:50:19 -0700

    WorkflowModel - better documentation on what's in validGraphTransitions

--- src/model/WorkflowModel.js
+++ src/model/WorkflowModel.js
@@ -10,6 +10,17 @@ Ext.define('Zan.data.model.WorkflowModel', {
         { name: 'name', type: 'string', allowNull: true },
         { name: 'marking', type: 'string', allowNull: true },
         { name: 'places' },
+        /*
+            todo: relationship / model?
+            An array of objects with properties:
+                name
+                froms -> string[] transition names
+                tos -> string[] transition names
+                blockers -> array of objects with properties
+                    code
+                    message
+                    parameters -> object with properties
+         */
         { name: 'validGraphTransitions' },
     ],
 
