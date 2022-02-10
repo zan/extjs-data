f9db30fecdffbf1661b02e39a7ca4c7d9617b68b
Alex Dean <alex@zanconsulting.com>
2022-02-10 10:10:27 -0700

    WorkflowButtonToolbarController - now reads label and submits json

--- src/view/workflow/WorkflowButtonToolbarController.js
+++ src/view/workflow/WorkflowButtonToolbarController.js
@@ -19,6 +19,7 @@ Ext.define('Zan.data.view.workflow.WorkflowButtonToolbarController', {
             var hidden = false;
             var disabled = false;
             var tooltip = null;
+
             if (item.blockers) {
                 disabled = true;
                 tooltip = [];
@@ -33,13 +34,13 @@ Ext.define('Zan.data.view.workflow.WorkflowButtonToolbarController', {
 
             items.push({
                 xtype: 'button',
-                text: item.name,
+                text: item.label,
                 margin: '0 5 0 5',
                 hidden: hidden,
                 disabled: disabled,
                 tooltip: tooltip,
                 handler: function(button) {
-                    this.doTransition(button.getText());
+                    this.doTransition(item.name);
                 },
                 scope: this
             });
@@ -48,9 +49,8 @@ Ext.define('Zan.data.view.workflow.WorkflowButtonToolbarController', {
         this.getView().add(items);
     },
 
-    doTransition: function(name) {
+    doTransition: async function(transitionId) {
         // todo: fire "before" events and check for responses
-        console.log("transition: %o", name);
 
         var entity = this.getView().getEntity();
         var entityClassName = Ext.ClassManager.get(entity.$className).entityClassName;
@@ -58,21 +58,11 @@ Ext.define('Zan.data.view.workflow.WorkflowButtonToolbarController', {
         // todo: this should be configurable
         var url = '/api/zan/drest/entity-workflow/' + entityClassName + '/' + entity.getId() + '/transition';
 
-        var params = {
-            newPlace: name,
-        };
+        await Zan.data.Api.post(url, { transition: transitionId });
 
-        // todo: make this show up in the rest debugger
-        Ext.Ajax.request({
-            url: url,
-            method: 'POST',
-            params: params,
-            success: function(response, opts) {
-                // todo: this might need more API parameters, should be an event instead?
-                entity.load({
-                    params: { includeWorkflow: true }
-                });
-            },
+        // Refresh the entity
+        entity.load({
+            params: { includeWorkflow: true }
         });
 
         // todo: fire "after" events
