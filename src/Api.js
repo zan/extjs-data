213e3c5596b321e3df0d964cb02b1e41f549b53e
Alex Dean <alex@zanconsulting.com>
2022-02-10 17:15:01 -0700

    Api.js - better error handling

--- src/Api.js
+++ src/Api.js
@@ -31,11 +31,13 @@ Ext.define('Zan.data.Api', {
             method: method,
             parameters: params,
         })[0];
-        console.log("rest request is: %o", debugRestRequest);
 
         var requestConfig = {
-            method: method,
             url: url,
+            method: method,
+            headers: {
+                'Accept': 'application/json',
+            },
             disableCaching: false,
 
             success: (response, opts) => {
@@ -51,7 +53,12 @@ Ext.define('Zan.data.Api', {
                 debugRestRequest.set('isError', true);
 
                 // todo: return json error response from the server
-                deferred.reject("Check the network tab for more details");
+                //deferred.reject("Check the network tab for more details");
+                var error = new ZanDataApiError(
+                    "API call failed: '" + responseInfo.errorMessage + "'. See the network tab for more details.",
+                    responseInfo
+                );
+                deferred.reject(error);
             },
 
             callback: function(options, wasSuccessful, response) {
@@ -69,25 +76,59 @@ Ext.define('Zan.data.Api', {
         return deferred.promise;
     },
 
-    _parseRawResponse: function(response) {
+    _parseRawResponse: function(rawResponse) {
         var responseInfo = {
             requestSuccessful: null,
             responseData: null,
             sfProfilerToken: null,
             sfProfilerUrl: null,
+            errorMessage: 'Unknown Error',  // Better error message is parsed out below
+            errorCode: null,
         };
 
-        responseInfo.sfProfilerToken = response.getResponseHeader('X-Debug-Token');
-        responseInfo.sfProfilerUrl = response.getResponseHeader('X-Debug-Token-Link');
-
-        var responseData = response.responseText;
-
-        if (response.getResponseHeader('Content-Type') === 'application/json') {
-            responseData = JSON.parse(response.responseText);
+        var responseData = rawResponse.responseText;
+        if (rawResponse.getResponseHeader('Content-Type') === 'application/json') {
+            responseData = JSON.parse(rawResponse.responseText);
         }
+
+        // Check for errors
+        if (rawResponse.status >= 500 && rawResponse.status <= 599) {
+            responseInfo.requestSuccessful = false;
+            if (responseData.detail) responseInfo.errorMessage = responseData.detail;
+        }
+
+        responseInfo.sfProfilerToken = rawResponse.getResponseHeader('X-Debug-Token');
+        responseInfo.sfProfilerUrl = rawResponse.getResponseHeader('X-Debug-Token-Link');
 
         responseInfo.responseData = responseData;
 
+        responseInfo.errorCode = this._resolveErrorCode(responseData, responseInfo);
+
         return responseInfo;
     },
+
+    /**
+     * Attempts to return an error code for known error situations
+     */
+    _resolveErrorCode: function(responseData, responseInfo) {
+        // Easy case: rawResponse includes an errorCode
+        if (responseData.errorCode) return responseData.errorCode;
+
+        return null;    // return null if there weren't any matches
+    },
 });
+
+
+// todo: can this move to overrides or some other file?
+class ZanDataApiError extends Error {
+    constructor(message, responseInfo) {
+        super(message);
+
+        this.name = "ZanDataApiError";
+        this.responseInfo = responseInfo;
+    }
+
+    getResponseInfo() {
+        return this.responseInfo;
+    }
+}
