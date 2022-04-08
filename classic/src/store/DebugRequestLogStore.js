d6e0e9b288256733c5b62f9807068d47c2c0b5da
Alex Dean <alex@zanconsulting.com>
2022-04-08 16:40:14 -0600

    DebugRequestLogStore - moved logic for adding a record from a request into the store

--- classic/src/store/DebugRequestLogStore.js
+++ classic/src/store/DebugRequestLogStore.js
@@ -11,5 +11,26 @@ Ext.define('Zan.data.store.DebugRequestLogStore', {
     model: 'Zan.data.model.RestRequestModel',
     sorters: [
         { property: 'responseAt', direction: 'DESC' },
-    ]
+    ],
+
+    /**
+     *
+     */
+    addFromRawRequest: function(request, success) {
+        var url = request.getUrl();
+
+        // URL does not include query parameters for GET requests, add them back in
+        if ('GET' === request.getMethod() && !Ext.isEmpty(request.getParams())) {
+            var encodedParams = Ext.Object.toQueryString(request.getParams());
+            url = url + '?' + encodedParams;
+        }
+
+        this.add({
+            responseAt: new Date(),
+            isError: !success,
+            method: request.getMethod(),
+            url: url,
+            parameters: JSON.stringify(request.getJsonData()),
+        });
+    }
 });
