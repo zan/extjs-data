c45798f088a105fad9572bf1802e5df94b2d768c
Alex Dean <alex@zanconsulting.com>
2022-05-09 14:49:38 -0600

    RecordFormPanel - register with viewModel if 'reference' is set

--- classic/src/form/RecordFormPanel.js
+++ classic/src/form/RecordFormPanel.js
@@ -1,5 +1,24 @@
+/**
+ * Form panel with model integration
+ *
+ * If this form is created with a reference it will register itself with the viewModel
+
+ ### Examples
+
+    {
+       xtype: 'zan-recordformpanel',
+       reference: 'myForm', // This will register the form with the viewModel
+       bind: {
+           formRecord: '{myRecord}',
+       }
+    },
+
+
+ */
 Ext.define('Zan.data.form.RecordFormPanel', {
     extend: 'Ext.form.Panel',
+
+    alias: 'widget.zan-recordformpanel',
 
     requires: [
         'Zan.data.form.RecordFormMixin',
@@ -21,5 +40,14 @@ Ext.define('Zan.data.form.RecordFormPanel', {
 
     updateFormRecord: function(record) {
         this.loadRecord(record);
+    },
+
+    initComponent: function() {
+        this.callParent(arguments);
+
+        // If this component has a reference and a viewModel register it with that viewModel
+        if (this.getReference() && this.lookupViewModel()) {
+            this.lookupViewModel().set(this.getReference(), this);
+        }
     },
 });
