f067b02f6e93739fd804be3b98614933236dba67
Alex Dean <alex@zanconsulting.com>
2022-05-10 13:18:53 -0600

    Boolean column - add nullText config

--- classic/src/grid/column/Boolean.js
+++ classic/src/grid/column/Boolean.js
@@ -2,10 +2,26 @@ Ext.define('Zan.data.grid.column.Boolean', {
     extend: 'Ext.grid.column.Boolean',
     alias: 'widget.zan-booleancolumn',
 
+    config: {
+        /**
+         * @cfg string Text to use when the value being rendered is null
+         */
+        nullText: '',
+    },
+
     width: 100,
 
     trueText: 'Yes',
     falseText: 'No',
 
     filterType: 'boolean',
+
+    /**
+     * OVERRIDDEN to support nullText option
+     */
+    defaultRenderer: function(value) {
+        if (value === null) return this.getNullText();
+
+        return this.callParent([ value ]);
+    },
 });
