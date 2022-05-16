396339b33a54306885561dbf3eddd0cd08d15be9
Alex Dean <alex@zanconsulting.com>
2022-05-16 16:25:57 -0600

    Api.js - better error handling

--- src/Api.js
+++ src/Api.js
@@ -19,6 +19,46 @@ Ext.define('Zan.data.Api', {
         return rawResult.responseData;
     },
 
+    /**
+     * Displays an API error to the user in a popup
+     *
+     * @param {ZanDataApiError} ex
+     */
+    displayApiError: function(ex) {
+        var title = 'Server Error';
+        var message = '';
+        var responseInfo = ex.getResponseInfo();
+
+        // 409: Conflict (user's view of the data is out of date)
+        if (responseInfo.httpStatus === 409) {
+            title = 'Conflicting Edits';
+
+            message = [
+                'Changes were made to this record that would conflict with your changes.',
+                'Please refresh the page and try again.',
+            ].join("<p>");
+        }
+        // Pull the error from the response information
+        else {
+            message = [
+                'There was a problem handling your request',
+                'Technical details: HTTP ' + Ext.htmlEncode(responseInfo.httpStatus + ': ' + responseInfo.httpStatusText),
+            ].join("<p>");
+        }
+
+        Ext.create('Zan.common.view.PopupDialogPanel', {
+            title: title,
+            showCancelButton: false,
+            bodyPadding: 8,
+            items: [
+                {
+                    xtype: 'panel',
+                    html: message,
+                },
+            ]
+        });
+    },
+
     makeRequest: async function(url, method, params) {
         var me = this;
         var deferred = new Ext.Deferred();
@@ -93,7 +133,7 @@ Ext.define('Zan.data.Api', {
             responseData: null,
             sfProfilerToken: null,
             sfProfilerUrl: null,
-            errorMessage: 'Unknown Error',  // Better error message is parsed out below
+            errorMessage: rawResponse.errorMessage,  // Additional checks for this field below
             errorCode: null,
         };
 
@@ -103,9 +143,10 @@ Ext.define('Zan.data.Api', {
         }
 
         // Check for errors
-        if (rawResponse.status >= 500 && rawResponse.status <= 599) {
+        if (rawResponse.status >= 400 && rawResponse.status <= 599) {
             responseInfo.requestSuccessful = false;
-            if (responseData.detail) responseInfo.errorMessage = responseData.detail;
+            responseInfo.errorMessage = responseData.errorMessage;
+            if (!responseInfo.errorMessage && rawResponse.detail) responseInfo.errorMessage = responseData.detail;
         }
 
         responseInfo.sfProfilerToken = rawResponse.getResponseHeader('X-Debug-Token');
@@ -114,6 +155,11 @@ Ext.define('Zan.data.Api', {
         responseInfo.responseData = responseData;
 
         responseInfo.errorCode = this._resolveErrorCode(responseData, responseInfo);
+
+        // Fall back an unknown error
+        if (!responseInfo.requestSuccessful && !responseInfo.errorMessage) {
+            responseInfo.errorMessage = 'Unknown Error';
+        }
 
         return responseInfo;
     },
