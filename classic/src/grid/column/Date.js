c94e8b61bc9e81ae1aeeaea737470ea44634c8e1
Alex Dean <alex@zanconsulting.com>
2022-07-26 14:35:23 -0600

    Date and DateTime columns now support filtering "onday" to find any records that match the given day

--- classic/src/grid/column/Date.js
+++ classic/src/grid/column/Date.js
@@ -4,7 +4,26 @@ Ext.define('Zan.data.grid.column.Date', {
 
     width: 100,
 
-    format: Ext.Date.defaultFormat,
+    filterType: {
+        type: 'date',
+        fieldDefaults: {
+            xtype: 'datefield',
+        },
 
-    filterType: 'date',
+        operators: ['onday', 'notonday', '>', '>=', '<', '<=', 'empty', 'nempty' ],
+        operator: 'onday', // default operator
+    },
+
+    constructor: function(config) {
+        // Add additional supported operators
+        Ext.grid.plugin.filterbar.Operator.addOperator('onday', 'x-fa fa-calendar-day', 'On Day');
+        Ext.grid.plugin.filterbar.Operator.addOperator('notonday', 'x-fa fa-calendar-times', 'Not On Day');
+
+        // Must be done in the controller because this property may be updated after this component is defined
+        config = Ext.applyIf(config, {
+            format: Ext.Date.defaultFormat,
+        });
+
+        this.callParent([ config ]);
+    },
 });
--- classic/src/grid/column/DateTime.js
+++ classic/src/grid/column/DateTime.js
@@ -4,15 +4,27 @@ Ext.define('Zan.data.grid.column.DateTime', {
 
     width: 150,
 
-    filterType: 'date',
+    filterType: {
+        type: 'date',
+        fieldDefaults: {
+            xtype: 'datefield',
+        },
+
+        operators: ['onday', 'notonday', '>', '>=', '<', '<=', 'empty', 'nempty' ],
+        operator: 'onday', // default operator
+    },
 
     constructor: function(config) {
+        // Add additional supported operators
+        Ext.grid.plugin.filterbar.Operator.addOperator('onday', 'x-fa fa-calendar-day', 'On Day');
+        Ext.grid.plugin.filterbar.Operator.addOperator('notonday', 'x-fa fa-calendar-times', 'Not On Day');
+
         // Must be done in the controller because these properties may be updated after this component is defined
         config = Ext.applyIf(config, {
             format: Ext.Date.defaultFormat + ' ' + Ext.Date.defaultTimeFormat,
         });
 
-        this.callParent([ config ])
+        this.callParent([ config ]);
     },
 
 });
